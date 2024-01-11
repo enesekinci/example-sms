60632ea83c6fb945ea9ad4b575dfdd4cee77286c
Enes EKİNCİ <enes.ekinci@turkticaret.net>
2024-01-11 13:23:04 +0300

    refresh token swagger documentation

--- app/Http/Controllers/Swagger/MainController.php
+++ app/Http/Controllers/Swagger/MainController.php
@@ -3,39 +3,19 @@
 namespace App\Http\Controllers\Swagger;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use OpenApi\Annotations as OA;
 
 
 /**
  * @OA\Info(
  *     version="1.0",
- *     title="Example for response examples value"
+ *     title="SMS API",
  * )
  *
  * @OA\PathItem(path="/api")
  *
- * @OA\Put(
- *     tags={"Users"},
- *     path="/users/{id}",
- *     summary="Updates a user",
- *     @OA\Parameter(
- *         description="Parameter with mutliple examples",
- *         in="path",
- *         name="id",
- *         required=true,
- *         @OA\Schema(type="string"),
- *         @OA\Examples(example="int", value="1", summary="An int value."),
- *         @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
- *     ),
- *     @OA\Response(
- *         response=200,
- *         description="OK"
- *     )
- * )
+ *
  *
  */
-
 class MainController extends Controller
 {
     //
--- app/Http/Controllers/V1/Auth/JWT/RefreshTokenController.php
+++ app/Http/Controllers/V1/Auth/JWT/RefreshTokenController.php
@@ -12,6 +12,54 @@ use App\Http\Resources\UserResource;
 use Exception;
 use Illuminate\Http\JsonResponse;
 
+/**
+ * RefreshToken
+ *
+ * @OA\Post(
+ *     path="/auth/token/generate/refresh-token",
+ *     tags={"Auth"},
+ *     summary="Refresh token",
+ *     operationId="refreshToken",
+ *     @OA\RequestBody(
+ *     description="Refresh token",
+ *     required=true,
+ *     @OA\JsonContent(
+ *     required={"email", "password"},
+ *     @OA\Property(property="email", type="string", example="test@test.com"),
+ *     @OA\Property(property="password", type="string", example="password"),
+ *     ),
+ *     ),
+ *     @OA\Response(
+ *     response=200,
+ *     description="Success",
+ *     @OA\JsonContent(
+ *     @OA\Property(property="success", type="boolean", example=true),
+ *     @OA\Property(property="type", type="string", example="refresh"),
+ *     @OA\Property(property="token", type="string", example="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"),
+ *     @OA\Property(property="user", type="object"),
+ *     ),
+ *     ),
+ *     @OA\Response(
+ *     response=401,
+ *     description="Unauthorized",
+ *     @OA\JsonContent(
+ *     @OA\Property(property="message", type="string", example="User is not authorized"),
+ *     ),
+ *     ),
+ *     @OA\Response(
+ *     response=422,
+ *     description="Unprocessable Entity",
+ *     @OA\JsonContent(
+ *     @OA\Property(property="message", type="string", example="The given data was invalid."),
+ *     @OA\Property(property="errors", type="object"),
+ *     ),
+ *     ),
+ *     security={
+ *     {"bearerAuth": {}}
+ *     }
+ *     )
+ *
+ */
 class RefreshTokenController extends Controller
 {
     /**
